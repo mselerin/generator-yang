ab4116d313fa5ead03627ee28d65b08e46e31563
Michel Selerin <michel.selerin@outlook.com>
2017-02-16 17:29:43 +0100

    Clean Webpack Plugin

--- generators/app/templates/webpack.config.js
+++ generators/app/templates/webpack.config.js
@@ -8,6 +8,7 @@ const merge = require('webpack-merge');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 
 const ENV_CONFIG = require('./env-config');
@@ -25,111 +26,111 @@ PATHS.core         = path.join(PATHS.app, 'core');
 
 const baseOutputName = `[name]-${APP_VERSION}`;
 const extractCSS = new ExtractTextPlugin({
-    filename: `${baseOutputName}.css`
+   filename: `${baseOutputName}.css`
 });
 
 
 const baseConfig = {
-    devtool: false,
+   devtool: false,
 
-    entry: {
-        'main': path.join(PATHS.app, 'main.ts')
-    },
-
-
-    output: {
-        path: PATHS.dist,
-        publicPath: '',
-        filename: `${baseOutputName}.js`
-    },
+   entry: {
+      'main': path.join(PATHS.app, 'main.ts')
+   },
 
 
-    resolve: {
-        modules: ['node_modules'],
-        extensions: ['.ts', '.js'],
-        alias: {
-            'app': PATHS.app,
-            'resources': PATHS.resources,
-            'core': PATHS.core
-        }
-    },
+   output: {
+      path: PATHS.dist,
+      publicPath: '/',
+      filename: `${baseOutputName}.js`
+   },
 
 
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                loaders: ['ts-loader', 'angular2-template-loader'],
-                exclude: [/\.(spec|e2e)\.ts$/]
-            },
+   resolve: {
+      modules: ['node_modules'],
+      extensions: ['.ts', '.js'],
+      alias: {
+         'app': PATHS.app,
+         'resources': PATHS.resources,
+         'core': PATHS.core
+      }
+   },
 
-            {
-                test: /\.s?css$/,
-                loader: extractCSS.extract({
-                    fallback: 'style-loader',
-                    use: ['css-loader', 'sass-loader']
-                }),
-                exclude: [/\.component\.s?css$/]
-            },
 
-            {
-                test: /\.s?css$/,
-                loaders: ['to-string-loader', 'css-loader', 'sass-loader'],
-                include: [/\.component\.s?css$/]
-            },
+   module: {
+      rules: [
+         {
+            test: /\.ts$/,
+            loaders: ['ts-loader', 'angular2-template-loader'],
+            exclude: [/\.(spec|e2e)\.ts$/]
+         },
 
-            { test: /\.json/, loader: 'json-loader' },
-            { test: /\.html$/, loader: 'html-loader' },
+         {
+            test: /\.s?css$/,
+            loader: extractCSS.extract({
+               fallback: 'style-loader',
+               use: ['css-loader', 'sass-loader']
+            }),
+            exclude: [/\.component\.s?css$/]
+         },
 
-            {
-                test: /\.(png|jpe?g|gif|svg|woff2?|eot|ttf|otf)(\?.*)?$/,
-                loader: 'file-loader',
-                query: {
-                    name: `[path][name].[ext]`
-                }
+         {
+            test: /\.s?css$/,
+            loaders: ['to-string-loader', 'css-loader', 'sass-loader'],
+            include: [/\.component\.s?css$/]
+         },
+
+         { test: /\.json/, loader: 'json-loader' },
+         { test: /\.html$/, loader: 'html-loader' },
+
+         {
+            test: /\.(png|jpe?g|gif|svg|woff2?|eot|ttf|otf)(\?.*)?$/,
+            loader: 'file-loader',
+            query: {
+               name: `[path][name].[ext]`
             }
-        ]
-    },
+         }
+      ]
+   },
 
-    plugins: [
-        // Permet de séparer le code applicatif des librairies externes
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'vendor',
-            minChunks: (module) => {
-                const userRequest = module.userRequest;
-                return (userRequest && userRequest.indexOf(PATHS.node_modules) >= 0);
-            }
-        }),
+   plugins: [
+      // Permet de séparer le code applicatif des librairies externes
+      new webpack.optimize.CommonsChunkPlugin({
+         name: 'vendor',
+         minChunks: (module) => {
+            const userRequest = module.userRequest;
+            return (userRequest && userRequest.indexOf(PATHS.node_modules) >= 0);
+         }
+      }),
 
-        new webpack.NoEmitOnErrorsPlugin(),
+      new webpack.NoEmitOnErrorsPlugin(),
 
-        new webpack.DefinePlugin({
-            CLIENT_CONFIG: JSON.stringify(profileConfig.clientConfig)
-        }),
+      new webpack.DefinePlugin({
+         CLIENT_CONFIG: JSON.stringify(profileConfig.clientConfig)
+      }),
 
-        new webpack.ProvidePlugin({
-            $: 'jquery', jquery: 'jquery', jQuery: 'jquery', 'window.jQuery' : 'jquery'
-        }),
+      new webpack.ProvidePlugin({
+         $: 'jquery', jquery: 'jquery', jQuery: 'jquery', 'window.jQuery' : 'jquery'
+      }),
 
-        extractCSS,
+      extractCSS,
 
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: 'app/index.html',
-            favicon: 'app/favicon.png',
-            inject: true,
-            minify: false
-        }),
+      new HtmlWebpackPlugin({
+         filename: 'index.html',
+         template: 'app/index.html',
+         favicon: 'app/favicon.png',
+         inject: true,
+         minify: false
+      }),
 
-        // Recopier tout sauf ce qui est déjà pris en compte
-        new CopyWebpackPlugin([
-            {
-                from: 'app',
-                to: 'app',
-                ignore: ['*.ts', '*.scss', '*.css', '*.html']
-            }
-        ])
-    ]
+      // Recopier tout sauf ce qui est déjà pris en compte
+      new CopyWebpackPlugin([
+         {
+            from: 'app',
+            to: 'app',
+            ignore: ['*.ts', '*.scss', '*.css', '*.html']
+         }
+      ])
+   ]
 
 };
 
@@ -137,25 +138,25 @@ const baseConfig = {
 let config = merge(baseConfig, {});
 
 if ('dev' === PROFILE) {
-    config = merge(config, {
-        devtool: 'cheap-module-eval-source-map',
+   config = merge(config, {
+      devtool: 'cheap-module-eval-source-map',
 
-        devServer: {
-            port: 3000,
-            inline: false,
-            hot: false,
-            stats: {
-                colors: true,
-                hash: false,
-                version: false,
-                assets: true,
-                modules: false,
-                children: false,
-                chunks: false,
-                chunkModules: false
-            }
-        }
-    });
+      devServer: {
+         port: 3000,
+         inline: false,
+         hot: false,
+         stats: {
+            colors: true,
+            hash: false,
+            version: false,
+            assets: true,
+            modules: false,
+            children: false,
+            chunks: false,
+            chunkModules: false
+         }
+      }
+   });
 }
 
 else {
@@ -173,8 +174,25 @@ else {
                screw_ie8: true,
                warnings: false
             }
-         })
-      ]
+         }),
+
+         new CleanWebpackPlugin([PATHS.dist])
+      ],
+
+      stats: {
+         colors: true,
+         hash: false,
+         version: false,
+         assets: true,
+         cached: false,
+         modules: false,
+         children: false,
+         chunks: false,
+         chunkModules: false,
+         chunkOrigins: false,
+         reasons: false,
+         source: false
+      }
    });
 }
 
