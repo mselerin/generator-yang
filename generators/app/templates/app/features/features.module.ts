d5765431f44b142f36e0db86edd2667a36c219d6
Michel Selerin <michel.selerin@outlook.com>
2017-03-04 09:17:46 +0100

    Yang-needles for future code injection

--- generators/app/templates/app/features/features.module.ts
+++ generators/app/templates/app/features/features.module.ts
@@ -2,16 +2,17 @@ import {NgModule} from "@angular/core";
 
 import {LayoutModule} from "app/features/layout/layout.module";
 import {HomeModule} from "app/features/home/home.module";
-
+/* yang-add-feature-import - Yang will add features imports here */
 
 const MODULES: any[] = [
-    LayoutModule,
-    HomeModule
+   /* yang-add-feature-module - Yang will add features modules here */
+   LayoutModule,
+   HomeModule
 ];
 
 
 @NgModule({
-    imports: MODULES,
-    exports: MODULES
+   imports: MODULES,
+   exports: MODULES
 })
 export class Features {}
