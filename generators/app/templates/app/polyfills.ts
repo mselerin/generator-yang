6471a57f50c937231edb67eb01f211c9ebb59112
Michel Selerin <michel.selerin@outlook.com>
2017-06-01 12:09:13 +0200

    Deps update

--- generators/app/templates/app/polyfills.ts
+++ generators/app/templates/app/polyfills.ts
@@ -16,6 +16,5 @@ import 'core-js/es6/weak-set';
 import 'core-js/es6/typed';
 import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
-import 'zone.js/dist/zone';
-import 'reflect-metadata';
 import 'whatwg-fetch';
+import 'zone.js/dist/zone';
